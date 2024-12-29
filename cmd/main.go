140b5faf7ceb1c50274fdfb955c3bbc032dcf743
marj4 <mkasumov86@gmail.com>
2024-12-29 11:51:33 +0300

    1.Изменил константы 2.Написал функцию InfoForID() 3.Написал функцию DeleteFromID()

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	_ "github.com/lib/pq"
 	"library/config"
-	"library/internal"
 	"library/internal/db"
 	"log"
-	"os"
-	"strings"
+)
+
+const (
+	ConnectStatus = "Connect status:Succesful"
+	PingStatus    = "Ping status:Succesful"
 )
 
 func main() {
@@ -19,48 +20,52 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer DB.Close()
+
+	fmt.Println(ConnectStatus)
 
 	if err := db.Ping(DB); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Conneсt to DB is OK!")
+	fmt.Println(PingStatus)
 
-	scanOBJ := bufio.NewReader(os.Stdin)
+	//	fmt.Println("Enter the operation\n1.Book\n2.Books")
+	//	var operation int
+	//	fmt.Scan(&operation)
+	//	switch operation {
+	//	case 1:
+	//		fmt.Println("Enter:")
+	//		reader := bufio.NewReader(os.Stdin)
+	//
+	//		BookName, _ := reader.ReadString('\n')
+	//		BookName = strings.TrimSpace(BookName)
+	//
+	//		Author, _ := reader.ReadString('\n')
+	//		Author = strings.TrimSpace(BookName)
+	//
+	//		YearRealease, _ := reader.ReadString('\n')
+	//		YearRealease = strings.TrimSpace(BookName)
+	//
+	//		Book := &internal.Book{
+	//			BookName:     BookName,
+	//			Author:       Author,
+	//			YearRealease: YearRealease,
+	//		}
+	//
+	//		if err := db.InsertBook(DB, Book); err != nil {
+	//			log.Fatal(err)
+	//		}
+	//
+	//		fmt.Println("Insert status:succesful")
+	//	}
+	//
+	//info, err := db.InfoForID(DB, 3)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 
-	bookName, _ := scanOBJ.ReadString('\n')
-	bookName = strings.TrimSpace(bookName)
-
-	author, _ := scanOBJ.ReadString('\n')
-	author = strings.TrimSpace(author)
-
-	yearRealease, _ := scanOBJ.ReadString('\n')
-	yearRealease = strings.TrimSpace(yearRealease)
-
-	Book := &internal.Book{
-		BookName:     bookName,
-		Author:       author,
-		YearRealease: yearRealease,
-	}
-
-	if err := db.InsertBook(DB, Book); err != nil {
+	if err := db.DeleteFromID(DB, 7); err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Println("Date is insert succsesfully")
-
-	selectData, err := db.AllBooks(DB)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(selectData)
-
-	info, err := db.InfoForID(DB, 3)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(info)
+	fmt.Println("Delete is succesful!")
 
 }
--- internal/db/queries.go
+++ internal/db/queries.go
@@ -7,9 +7,11 @@ import (
 )
 
 const (
-	InsertError = "Cant insert data in db, %v"
-	OutputError = "Cant output data from db, %v"
-	WriteError  = "Cant  write data from db, %v"
+	ErrInsertData = "Can't insert data into the database: %v"
+	ErrOutputData = "Can't retrieve data from the database: %v"
+	ErrWriteData  = "Can't write data to the database: %v"
+	ErrQueryByID  = "Can't execute query by ID in the database: %v"
+	ErrDeleteByID = "Can't delete book by ID from the database: %v"
 )
 
 type Book struct {
@@ -22,7 +24,7 @@ type Book struct {
 func InsertBook(DB *sql.DB, Book *internal.Book) error {
 	query := `insert into books(bookName,author,yearrelease) values ($1,$2,$3)`
 	if _, err := DB.Exec(query, Book.BookName, Book.Author, Book.YearRealease); err != nil {
-		return fmt.Errorf(InsertError, err)
+		return fmt.Errorf(ErrInsertData, err)
 	}
 	return nil
 }
@@ -30,7 +32,7 @@ func AllBooks(DB *sql.DB) ([]Book, error) {
 	query := `select * from books`
 	rows, err := DB.Query(query)
 	if err != nil {
-		return nil, fmt.Errorf(OutputError, err)
+		return nil, fmt.Errorf(ErrOutputData, err)
 	}
 
 	var data []Book
@@ -38,7 +40,7 @@ func AllBooks(DB *sql.DB) ([]Book, error) {
 	for rows.Next() {
 		var book Book
 		if err := rows.Scan(&book.ID, &book.Name, &book.Author, &book.Year); err != nil {
-			return nil, fmt.Errorf(WriteError, err)
+			return nil, fmt.Errorf(ErrWriteData, err)
 		}
 
 		data = append(data, book)
@@ -47,5 +49,18 @@ func AllBooks(DB *sql.DB) ([]Book, error) {
 	return data, nil
 }
 func InfoForID(DB *sql.DB, id int) (Book, error) {
-
+	query := `select bookname,author,yearrelease from books where id=$1`
+	row := DB.QueryRow(query, id)
+	var book Book
+	if err := row.Scan(&book.Name, &book.Author, &book.Year); err != nil {
+		return Book{}, fmt.Errorf(ErrQueryByID, err)
+	}
+	return book, nil
+}
+func DeleteFromID(DB *sql.DB, id int) error {
+	query := `delete from books where id=$1`
+	if _, err := DB.Exec(query, id); err != nil {
+		return fmt.Errorf(ErrDeleteByID, err)
+	}
+	return nil
 }
